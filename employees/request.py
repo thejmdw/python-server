f30a8f10d00da01f064954cdf47543db0778961d
Jonathan Watson <thejmdw@gmail.com>
2021-07-13 13:07:59 -0400

    created employees and customers

--- employees/request.py
+++ employees/request.py
@@ -38,3 +38,10 @@ def get_single_employee(id):
             requested_employee = employee
 
     return requested_employee
+
+def create_employee(employee):
+  max_id = EMPLOYEES[-1]["id"]
+  new_id = max_id + 1
+  employee["id"] = new_id
+  EMPLOYEES.append(employee)
+  return employee
